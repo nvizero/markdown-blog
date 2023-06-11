c668e91c672247ecdf0d726b0c81e486a447d57b
victor.tseng <withvictor@gmail.com>
2023-06-11 12:40:34 +0800

    fix css and remove img

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -16,7 +16,7 @@ export default function PostPage({
           <h1 className="post-title">{title}</h1>
           <h3 className="post-title">{slug}</h3>
           <div className="post-date">Posted on {date}</div>
-          <img src={cover_image}/>
+          <img src={cover_image}  style={{"display":"none"}}/>
           <div className="post-body">
             <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
           </div>
